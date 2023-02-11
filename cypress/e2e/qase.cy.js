3ea044bbac33ad1cf28af41cac5dc675b7a50190
alex <aleksandar.stojanovic.91bgd@gmail.com>
2023-02-11 10:38:58 +0100

    alias

--- cypress/e2e/qase.cy.js
+++ cypress/e2e/qase.cy.js
@@ -1,5 +1,5 @@
 describe('QASE', () => {
-    it('Create a test project', () => {
+    xit('Create a test project', () => {
         cy.login(
             'aleksandar.stojanovic@qakursevionline.com',
             'TestPass123!');
@@ -10,7 +10,7 @@ describe('QASE', () => {
             'private',
             'all')
     })
-    it('Create a test suite', () => {
+    xit('Create a test suite', () => {
         cy.login(
             'aleksandar.stojanovic@qakursevionline.com',
             'TestPass123!');
@@ -22,7 +22,7 @@ describe('QASE', () => {
             'Test suite description',
             'Test preconditions')
     })
-    it('Create a test case', () => {
+    xit('Create a test case', () => {
 
         cy.login(
             'aleksandar.stojanovic@qakursevionline.com',
@@ -46,5 +46,27 @@ describe('QASE', () => {
             ['{"stepAction": "step action 1","data": "data 1","expectedResult": "expected result 1"}',
                 '{"stepAction": "step action 2","data": "data 2","expectedResult": "expected result 2"}',
                 '{"stepAction": "step action 3","data": "data 3","expectedResult": "expected result 3"}'])
+    })
+    it('Iterate through projects', () => {
+        cy.login(
+            'aleksandar.stojanovic@qakursevionline.com',
+            'TestPass123!');
+
+        // cy.get('.project-row > td:nth-child(4)').each(($el, index, $list)=>{
+        //     cy.log("Index " + index + " : " + $el.text())
+        // })
+
+        cy.get('.project-row > td:nth-child(4)').each(($el, index, $list)=>{
+            cy.wrap($el).invoke('text').as('elementText')
+            cy.get('@elementText').its('length').should('eq',16)
+            // cy.go('back')
+            // cy.go('foreward')
+            // cy.reload()
+            // cy.reload(true)
+        })
+
+
+        //cy.get('link koji otvara novi tab').invoke('removeAttr','target').click()
+
     })
 })
